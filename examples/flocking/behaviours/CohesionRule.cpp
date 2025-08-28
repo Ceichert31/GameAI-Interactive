45ae7b5771c4299b59c09a37c99b708f49df2029
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-08-28 11:53:37 -0400

    feat(cohesion): Boid distance check

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -6,6 +6,18 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
+  float centerOfMass = 0;
+
+  for (auto neighbor : neighborhood) {
+    //Divide sum of positions by number of neighbors
+      //Calculate distance between boids
+      Vector2f direction = neighbor->getPosition() - boid->getPosition();
+      float distance = direction.getMagnitude();
+
+      if (distance < neighbor->getDetectionRadius() ) {
+
+      }
+  }
 
   // find center of mass
 
