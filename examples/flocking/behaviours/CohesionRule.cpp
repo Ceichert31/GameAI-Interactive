35f1c155ef27c6e045811031823c4cccc0e14c16
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-08-28 16:44:17 -0400

    refactor(cohesion): Reduced amount of redundant code

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,27 +3,18 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
-
-  // todo: add your code here to make a force towards the center of mass
-  // hint: iterate over the neighborhood
-  Vector2f centerOfMass;
-  int numOfBoids = 0;
+  Vector2f centerOfMass{};
+  int numOfBoids{};
   float detectionRadius = boid->getDetectionRadius();
 
   for (auto neighbor : neighborhood) {
-      //Calculate distance between boids
-      Vector2f direction = neighbor->getPosition() - boid->getPosition();
-      float distance = direction.getMagnitude();
-
-      //Determine whether other boid is a neighbor
-      if (distance < detectionRadius) {
-        numOfBoids++;
-        centerOfMass += neighbor->getPosition();
-      }
-
-      centerOfMass = centerOfMass / numOfBoids;
+      numOfBoids++;
+      centerOfMass += neighbor->getPosition();
   }
+  //Calculate center of mass
+  centerOfMass = centerOfMass / numOfBoids;
 
+  //Set cohesion force
   cohesionForce = (boid->getPosition() - centerOfMass) / detectionRadius;
 
   return cohesionForce.normalized();
