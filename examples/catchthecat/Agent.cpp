94d158eaa22b12d0960975aa3e7099f6d2a34b0e
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-10-07 22:10:01 -0400

    feat: Implemented get neighbor function in generatePath function

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -24,15 +24,41 @@ std::vector<Point2D> Agent::generatePath(World* w) {
     // mark current as visited
     visited[currentPoint] = true;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
-
+    auto neighbors = getVisitableNeighbors(w, currentPoint, visited);
     // iterate over the neighs:
-    // for every neighbor set the cameFrom
-    // enqueue the neighbors to frontier and frontierset
+    for (auto neighbor : neighbors) {
+      // for every neighbor set the cameFrom
+      cameFrom[neighbor] = currentPoint;
+      // enqueue the neighbors to frontier and frontierset
+      frontier.push(neighbor);
+      frontierSet.insert(neighbor);
+    }
+
     // do this up to find a visitable border and break the loop
   }
 
   // if the border is not infinity, build the path from border to the cat using the camefrom map
+  if (borderExit != Point2D::INFINITE) {
+  }
+
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
   return vector<Point2D>();
+}
+std::vector<Point2D> Agent::getVisitableNeighbors(World* world, Point2D current, unordered_map<Point2D, bool> visited) {
+  std::vector<Point2D> neighbors;
+
+  //Check if in bounds
+  if (!world->isValidPosition(current)) {
+    return neighbors;
+  }
+
+  //Check if visited
+
+  //Check if cat
+  if (!world->catcherCanMoveToPosition(current)) {
+    return neighbors;
+  }
+
+  //Check if blocked
 }
