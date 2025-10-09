ede4759fa9bb09b9851ba11fd35880020e82d2ca
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-10-09 11:59:48 -0400

    chore: Added summaries and comments

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -6,26 +6,29 @@
 #include <queue>
 #include "World.h"
 using namespace std;
-std::vector<Point2D> Agent::generatePath(World* w) {
-  unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
-  unordered_map<Point2D, float> costSoFar;
-  priority_queue<WeightedPoint2D, vector<WeightedPoint2D>, greater<WeightedPoint2D>> frontier;                   // to store next ones to visit
-  unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
-  unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
 
-  // bootstrap state
+///<summary>
+/// Generates a path from the cat's current position to a border using the A* algorithm
+///</summary>
+///<returns> A vector of points leading to the closest border </returns>
+std::vector<Point2D> Agent::generatePath(World* w) {
+  unordered_map<Point2D, Point2D> cameFrom;
+  unordered_map<Point2D, float> costSoFar;
+  priority_queue<WeightedPoint2D, vector<WeightedPoint2D>, greater<WeightedPoint2D>> frontier;
+  unordered_set<Point2D> frontierSet;
+  unordered_map<Point2D, bool> visited;
+
+  //Initialization
   WeightedPoint2D catPos = WeightedPoint2D(w->getCat(), 1);
   frontier.push(catPos);
   costSoFar[catPos.point] = 0;
   frontierSet.insert(catPos.point);
-  Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
+  Point2D borderExit = Point2D::INFINITE;
 
   while (!frontier.empty()) {
-    // get the current from frontier
+    //Get the highest priority point from frontier
     auto currentPoint = frontier.top();
-    // remove the current from frontierset
     frontier.pop();
-    // mark current as visited
     visited[currentPoint.point] = true;
 
     //If algorithm hits border, set border exit
@@ -34,7 +37,7 @@ std::vector<Point2D> Agent::generatePath(World* w) {
       break;
     }
 
-    // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    //Get all visitable neighbors
     auto neighbors = getVisitableNeighbors(w, currentPoint.point, visited, frontierSet);
 
     //If there are no valid neighbors, continue
@@ -42,7 +45,7 @@ std::vector<Point2D> Agent::generatePath(World* w) {
       continue;
     }
 
-    // iterate over the neighs:
+    //Add visitable neighbors to frontier and calculate cost
     for (auto neighbor : neighbors) {
       //Calculate new cost (So far just add one because every tile costs 1)
       float newCost = costSoFar[currentPoint.point] + 1;
@@ -52,18 +55,17 @@ std::vector<Point2D> Agent::generatePath(World* w) {
       {
         costSoFar[neighbor] = newCost;
 
+        //Calculate priority
         float priority = newCost + w->heuristic(neighbor);
-
-        // for every neighbor set the cameFrom
         cameFrom[neighbor] = currentPoint.point;
-        // enqueue the neighbors to frontier and frontierset
+
+        //Add to frontier w/ priority
         frontier.push(WeightedPoint2D(neighbor, priority));
         frontierSet.insert(neighbor);
       }
     }
   }
-
-  // if the border is not infinity, build the path from border to the cat using the camefrom map
+  //If there is an exit, calculate path
   if (borderExit != Point2D::INFINITE) {
     vector<Point2D> path;
 
@@ -78,11 +80,15 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 
     return path;
   }
-
-  // if there isnt a reachable border, just return empty vector
-  // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
+  //No exit found
   return vector<Point2D>();
 }
+
+#pragma region Neighbor Functions
+///<summary>
+/// Checks which neighbors to the passed through point are visitable
+///</summary>
+///<returns> A vector of neighbors which are visitable </returns>
 std::vector<Point2D> Agent::getVisitableNeighbors(World* world, Point2D current, const std::unordered_map<Point2D, bool>& visited, const std::unordered_set<Point2D>& frontierSet) {
   std::vector<Point2D> neighbors;
 
@@ -113,6 +119,15 @@ std::vector<Point2D> Agent::getVisitableNeighbors(World* world, Point2D current,
 
   return neighbors;
 }
+
+///<summary>
+/// Checks if a certain neighbor is a valid position
+///</summary>
+/// <remarks> A valid position needs: To not be the cat's position,
+/// to be inside the grid, to not be blocked,
+/// to not have been visited,
+/// to not be in frontier</remarks>
+///<returns> true if the neighbor meets all requirements to be valid </returns>
 bool Agent::isNeighborValid(World* w, Point2D current, const std::unordered_map<Point2D, bool>& visited, const std::unordered_set<Point2D>& frontierSet) {
   //Check if cat
   if (w->getCat() == current) {
@@ -124,6 +139,7 @@ bool Agent::isNeighborValid(World* w, Point2D current, const std::unordered_map<
     return false;
   }
 
+  //Check if blocked
   if (w->getContent(current)) {
     return false;
   }
@@ -140,4 +156,4 @@ bool Agent::isNeighborValid(World* w, Point2D current, const std::unordered_map<
 
   return true;
 }
-
+#pragma endregion
